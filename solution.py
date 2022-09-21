3c22491c71fd83c4cf2ad92275616b57f7c4f6dc
Shan Amir <mrshanamir01@gmail.com>
2022-09-21 19:23:34 -0400

    Attempt 1

--- solution.py
+++ solution.py
@@ -1,48 +1,64 @@
-### welcome_assignment_answers
-### Input - All nine questions given in the assignment.
-### Output - The right answer for the specific question.
+# import socket module
+from socket import *
+# In order to terminate the program
+import sys
 
-def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
-    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "mtls"
-    elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes"
-    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No"
-    elif question == "What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-        answer = "8496abe9fceb5aa927e28bfbd9a2347d1290ef9b"
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "No"
-    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 5
-    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = 3
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        answer = ""
-    return(answer)
-# Complete all the questions.
+
+def webServer(port=13331):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+
+    # Prepare a server socket
+    serverSocket.bind(("", port))
+
+    # Fill in start
+    serverSocket.listen(1)
+    # Fill in end
+
+    while True:
+        # Establish the connection
+
+        print('Ready to serve...')
+        connectionSocket, addr =  serverSocket.accept()
+
+        try:
+            message = serverSocket.recv(1024).decode()
+            filename = message.split()[1]
+
+            # opens the client requested file.
+            # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
+            f = open(filename[1:], "r")
+
+            outputdata=b"Content-Type: text/html; charset=UTF-8\r\n" \
+                    # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
+            # Content-Type above is an example on how to send a header as bytes
+            # Fill in end
+
+            # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
+            # Fill in start
+            validResponse=b"HTTP/1.1 200 OK"
+            # Fill in end
+
+            # Send the content of the requested file to the client
+            for i in f:  # for line in file
+            # Fill in start - send your html file contents #Fill in end
+                connectionSocket.send(f.readline())
+                connectionSocket.close()  # closing the connection socket
+
+        except Exception as e:
+            connectionSocket.send(b"HTTP/1.1 404 Not Found")
+    # Send response message for invalid request due to the file not being found (404)
+    # Fill in start
+
+    # Fill in end
+
+    # Close client socket
+    # Fill in start
+
+    # Fill in end
+
+    serverSocket.close()
+    sys.exit()  # Terminate the program after sending the corresponding data
 
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-
-#Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA1 hashing value to the following message: 'NYU Computer Networking' - Use SHA1 hash generator and use the answer in your code":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+    webServer(13331)
